cfcc28ee33d6103ed793845996b35f6a20fe7db6
6201-Surface-1 <frc6201@gmail.com>
2023-10-12 16:40:08 -0400

    Create TankSubsystem.java

---
+++ ProgrammingIntro2023Code/src/main/java/frc/robot/Subsystems/TankSubsystem.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TankSubsystem extends SubsystemBase {
+  CANSparkMax left;
+  CANSparkMax right;
+  
+  /** Creates a new TankSubsystem. */
+  public TankSubsystem() {
+    left = new CANSparkMax(0, MotorType.kBrushless);
+    right = new CANSparkMax(1, MotorType.kBrushless);
+    
+  }
+
+  public void drive(double left, double right) {
+    this.left.set(left);
+    this.right.set(right);
+
+  }
+
+  public void stop() {
+    left.set(0);
+    right.set(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
+
