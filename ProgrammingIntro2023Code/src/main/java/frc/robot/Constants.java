3140fe2f4f4cb05ed680ab78ca6c442697a2f56a
6201-Surface-1 <frc6201@gmail.com>
2023-10-26 16:42:12 -0400

    Started AUTON and PID

--- ProgrammingIntro2023Code/src/main/java/frc/robot/Constants.java
+++ ProgrammingIntro2023Code/src/main/java/frc/robot/Constants.java
@@ -18,6 +18,16 @@ public final class Constants {
   }
 
   public static class DriveConstants {
-    
+   public static final double kg = 1/12.75;
+   public static final double kwc = 6 * Math.PI;
+   public static final double kd = kg * kwc;
+   
+   
+   public static final double kD = 1;
+   public static final double kP = 1;
+   public static final double kI = 1 ;
+
+   
+
   }
 }
--- ProgrammingIntro2023Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ ProgrammingIntro2023Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,22 +4,48 @@
 
 package frc.robot.subsystems;
 
+import javax.security.auth.RefreshFailedException;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DriveConstants;
 
 public class DriveSubsystem extends SubsystemBase {
+
+  PIDController drivController; 
+  //potatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopoatatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotatopotato
+
+
+
   CANSparkMax left;
+  RelativeEncoder leftEncoder;
   CANSparkMax right;
+  RelativeEncoder rightEncoder;
+
 
   /** Creates a new DriveSubsystem. */
   public DriveSubsystem() {
+
+drivController = new PIDController(p, i, d)
+
+
     left = new CANSparkMax(6, MotorType.kBrushless);
     //we have a left moror thats id is 1 and its brushless
+    leftEncoder = left.getEncoder();
+    leftEncoder.setPositionConversionFactor(DriveConstants.kd); 
+    leftEncoder.setPosition(0);
 
     right = new CANSparkMax(3, MotorType.kBrushless);
     //we have a right moror thats id is 2 and its brushless
+    rightEncoder = right.getEncoder();
+    rightEncoder.setPositionConversionFactor(DriveConstants.kd); 
+    rightEncoder.setPosition(0);
+
+
 
   }
 
